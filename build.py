b0c1849b3d0c1eef7d0c571a6ad56d7f7505c459
ading2210 <ading2019@gmail.com>
2023-08-08 00:33:09 -0700

    rebrand and build.py changes

--- build.py
+++ build.py
@@ -3,15 +3,11 @@ import pathlib
 import base64
 
 base_path = pathlib.Path(__file__).resolve().parent
-template_path = base_path / "template.html"
-out_path = base_path / "generated"
+template_path = base_path / "main.html"
+out_path = base_path / "data_url.txt"
 
 html = template_path.read_text()
-out_file = out_path / f"main.html"
-out_file.write_text(html)
-
 minified = minify_html.minify(html, minify_js=True, do_not_minify_doctype=True, minify_css=True)
 html_encoded = base64.b64encode(minified.encode()).decode()
 data_url = f"data:text/html;base64,{html_encoded}"
-data_file = out_path / "data_url.txt"
-data_file.write_text(data_url)
+out_path.write_text(data_url)
